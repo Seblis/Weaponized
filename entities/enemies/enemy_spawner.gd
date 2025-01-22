013aa65e7beb07d7e9ac2e3deb07aaa4f6c54f79
Duck <morlef@o2.pl>
2025-01-22 23:40:03 +0100

    Added enemy movement
    added enemy movement,
    added function for spawner

---
+++ entities/enemies/enemy_spawner.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var timer = $Timer
+@onready var spawn_point = $SpawnPoint
+
+var enemy_scene = preload("res://entities/enemies/enemy_body.tscn")
+
+func _ready() -> void:
+	timer.connect("timeout", _on_timeout)
+	
+func _on_timeout() -> void:
+	print("Spawning time")
+	spawn_enemy()
+	
+func spawn_enemy() -> void:
+	var enemy_instance = enemy_scene.instantiate()
+	enemy_instance.global_position = spawn_point.global_position
