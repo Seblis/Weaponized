419e9a2c9433b600ae823884ea49f929ebb4f7ee
Duck <morlef@o2.pl>
2025-01-25 23:12:33 +0100

    Created parallax background scene
    added background png file (cat_space)
    added parallax background scene

---
+++ levels/background/parallax_background.gd
@@ -0,0 +1,14 @@
+extends ParallaxBackground
+
+@export var background_texture = preload("res://levels/background/cat_space.png")
+@export var slide_speed = 20
+
+@onready var sprite = $ParallaxLayer/Sprite2D
+
+func _ready() -> void:
+	sprite.texture = background_texture
+	
+func _physics_process(delta: float) -> void:
+	sprite.region_rect.position += delta * Vector2(slide_speed,slide_speed)
+	if sprite.region_rect.position >= Vector2(1200,1200):
+		sprite.region_rect.position = Vector2.ZERO
