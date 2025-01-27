7b0ff98fe8dbbd14082d6e7af1d64d60c351de7d
Duck <morlef@o2.pl>
2025-01-27 22:41:03 +0100

    Updated parallax and frame
    changed parallax direction,
    changed Frame

--- levels/background/parallax_background.gd
+++ levels/background/parallax_background.gd
@@ -2,7 +2,7 @@ extends ParallaxBackground
 
 # if changed remember to update the preload file location path
 @export var background_texture = preload("res://levels/background/Acid_BG.png")
-@export var slide_speed = 20
+@export var slide_speed = 200
 
 @onready var sprite = $ParallaxLayer/Sprite2D
 
@@ -10,7 +10,7 @@ func _ready() -> void:
 	sprite.texture = background_texture
 	
 func _physics_process(delta: float) -> void:
-	sprite.region_rect.position.x += delta * slide_speed
+	sprite.region_rect.position -= delta * Vector2(0,slide_speed)
 	# pick correct numbers for Vector2 below
-	if sprite.region_rect.position >= Vector2(20000,200000):
+	if sprite.region_rect.position >= Vector2(1920,0):
 		sprite.region_rect.position = Vector2.ZERO
