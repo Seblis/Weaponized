7d0d30de9a18c59a805bd64f3dcf007d21bfaacd
Duck <morlef@o2.pl>
2025-01-27 20:37:21 +0100

    Added frames
    created framing

--- levels/background/parallax_background.gd
+++ levels/background/parallax_background.gd
@@ -1,7 +1,7 @@
 extends ParallaxBackground
 
 # if changed remember to update the preload file location path
-@export var background_texture = preload("res://levels/background/Acid_Side_BG.png")
+@export var background_texture = preload("res://levels/background/Acid_BG.png")
 @export var slide_speed = 20
 
 @onready var sprite = $ParallaxLayer/Sprite2D
@@ -10,7 +10,7 @@ func _ready() -> void:
 	sprite.texture = background_texture
 	
 func _physics_process(delta: float) -> void:
-	sprite.region_rect.position += delta * Vector2(slide_speed,slide_speed)
+	sprite.region_rect.position.x += delta * slide_speed
 	# pick correct numbers for Vector2 below
-	if sprite.region_rect.position >= Vector2(128,768):
+	if sprite.region_rect.position >= Vector2(20000,200000):
 		sprite.region_rect.position = Vector2.ZERO
