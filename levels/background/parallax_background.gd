2ab2f3ad590aaa3b34d977eda810e8b999a83454
Duck <morlef@o2.pl>
2025-01-27 20:11:58 +0100

    Added new textures
    added background textures

--- levels/background/parallax_background.gd
+++ levels/background/parallax_background.gd
@@ -1,7 +1,7 @@
 extends ParallaxBackground
 
 # if changed remember to update the preload file location path
-@export var background_texture = preload("res://levels/background/cat_space.png")
+@export var background_texture = preload("res://levels/background/Acid_Side_BG.png")
 @export var slide_speed = 20
 
 @onready var sprite = $ParallaxLayer/Sprite2D
@@ -12,5 +12,5 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	sprite.region_rect.position += delta * Vector2(slide_speed,slide_speed)
 	# pick correct numbers for Vector2 below
-	if sprite.region_rect.position >= Vector2(1920,1080):
+	if sprite.region_rect.position >= Vector2(128,768):
 		sprite.region_rect.position = Vector2.ZERO
