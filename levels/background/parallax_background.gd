de75375b6c8d14652e29298a582b9e3867e4d6a8
Duck <morlef@o2.pl>
2025-01-28 00:01:13 +0100

    Updated backgrounds and frame
    added all biome textures,
    fixed parallax background moving correctly upwards,
    changed texture mirroring/looping in project settings

--- levels/background/parallax_background.gd
+++ levels/background/parallax_background.gd
@@ -2,15 +2,20 @@ extends ParallaxBackground
 
 # if changed remember to update the preload file location path
 @export var background_texture = preload("res://levels/background/Acid_BG.png")
+@export var frame_texture: CompressedTexture2D
 @export var slide_speed = 200
 
 @onready var sprite = $ParallaxLayer/Sprite2D
-
+@onready var left_frame = $ParallaxFrames/LeftFrame
+@onready var right_frame = $ParallaxFrames/RightFrame
 func _ready() -> void:
 	sprite.texture = background_texture
-	
+	left_frame.texture = frame_texture
+	right_frame.texture = frame_texture
+	#frames.RightFrame.
 func _physics_process(delta: float) -> void:
-	sprite.region_rect.position -= delta * Vector2(0,slide_speed)
+	sprite.region_rect.position.y -= delta * slide_speed
 	# pick correct numbers for Vector2 below
-	if sprite.region_rect.position >= Vector2(1920,0):
+	if sprite.region_rect.position.y <= -2160:
 		sprite.region_rect.position = Vector2.ZERO
+		
