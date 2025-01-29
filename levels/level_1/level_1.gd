6a19be5deaa200a17efacae3932122d265a6a125
Bad-Grandpa <58433043+Bad-Grandpa@users.noreply.github.com>
2025-01-29 20:19:54 +0100

    fix
    fix

--- levels/level_1/level_1.gd
+++ levels/level_1/level_1.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var enemy_manager: EnemyManager = %EnemyManager
 
-var base_enemy = preload("res://entities/enemies/base_enemy/BaseEnemy.tscn")
+var base_enemy = preload("res://entities/enemies/base_enemy/base_enemy.tscn")
 var square_enemy = preload("res://entities/enemies/square/square_enemy.tscn")
 var triangle_enemy = preload("res://entities/enemies/triangle/triangle_enemy.tscn")
 var rapidfire_enemy = preload("res://entities/enemies/rapidfire/rapidfire_enemy.tscn")
@@ -17,9 +17,11 @@ var rapidfire_enemy = preload("res://entities/enemies/rapidfire/rapidfire_enemy.
 func _ready():
 	var waves = [
 		[[rapidfire_enemy, 1, 3, 2, 0.07]],
+		[[triangle_enemy, 1, 3, 2, 0.07]],
 		[[triangle_enemy, 0, 3, 1, 0.07]],
 		[[base_enemy, 1, 3, 1, 0.07]],
-		[[square_enemy, 0, 3, 1, 0.07],[triangle_enemy, 1, 3, 1, 0.07]]
+		[[square_enemy, 0, 3, 1, 0.07]
+		,[triangle_enemy, 1, 3, 1, 0.07]]
 		
 	]
 	enemy_manager.supply_waves(waves)
