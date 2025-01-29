dff9e2fa2eaf1c6b307bd5872ef70bd97d048c26
Sebastian <seblis91@gmail.com>
2025-01-29 15:36:26 +0100

    Reload feature added for Rapidfire enemy

--- entities/enemies/rapidfire/weapon_manager.gd
+++ entities/enemies/rapidfire/weapon_manager.gd
@@ -1,12 +1,20 @@
 extends Node2D
 
 @onready var shooting_point = $ShootingPoint
+@onready var reload = %Reload
+@onready var weapon_cooldown = %WeaponCooldown
 
 @export var shooting_angle = 0.35
+@export var magazine_size = 10
+
+var _magazine_state = 0
 
 var base_bullet = preload("res://entities/enemies/common/base_bullet.tscn")
 var _last_position: Vector2
 var _motion: Vector2
+
+func _ready():
+	_magazine_state = magazine_size
 
 func _physics_process(delta):
 	_motion = (global_position - _last_position) / delta
@@ -25,5 +33,12 @@ func shoot(angle = 0.0):
 	
 	get_tree().current_scene.add_child(new_bullet)
 	
+	_magazine_state -= 1
+	if not _magazine_state:
+		weapon_cooldown.stop()
+		reload.start()
 
-	
+func _on_reload_timeout():
+	_magazine_state = magazine_size
+	shoot()
+	weapon_cooldown.start()
