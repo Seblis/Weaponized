25d52c5a718bc1b3ea92cf5aecec3cc7f2d1a2bd
Duck <morlef@o2.pl>
2025-01-29 18:59:15 +0100

    Created audio manager
    added audio files,
    created variables to connect with audio files,
    set audio manager scene as autoload

--- util/audio_manager.gd
+++ util/audio_manager.gd
@@ -1,1 +1,7 @@
 extends Node
+
+@onready var projectile_sfx: AudioStreamPlayer = $ProjectileSFX
+@onready var level_sfx: AudioStreamPlayer = $LevelSFX
+@onready var menu_sfx: AudioStreamPlayer = $MenuSFX
+@onready var level_complete_sfx: AudioStreamPlayer = $LevelCompleteSFX
+@onready var game_over_sfx: AudioStreamPlayer = $GameOverSFX
