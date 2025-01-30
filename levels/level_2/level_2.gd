890dd55c228106077a9f80d63b0184092d5d954b
Bad-Grandpa <58433043+Bad-Grandpa@users.noreply.github.com>
2025-01-30 01:33:27 +0100

    update
    update

--- levels/level_2/level_2.gd
+++ levels/level_2/level_2.gd
@@ -1,38 +1,40 @@
 extends Node2D
+
 @onready var enemy_manager: EnemyManager = %EnemyManager
-@onready var pause_menu = $CanvasLayer/PauseMenu
-var base_enemy = preload("res://entities/enemies/BaseEnemy.tscn")
+
+var base_enemy = preload("res://entities/enemies/base_enemy/base_enemy.tscn")
+var square_enemy = preload("res://entities/enemies/square/square_enemy.tscn")
+var triangle_enemy = preload("res://entities/enemies/triangle/triangle_enemy.tscn")
+var rapidfire_enemy = preload("res://entities/enemies/rapidfire/rapidfire_enemy.tscn")
 
 ##
 #  [enemy_scene, path_id, enemies_amount, delay(s), spacing]
 #  we can provide optional array with enemy parameters at the end
 ##
 
-var waves = [
-	[	
-		#wave 1
-		[base_enemy, 0, 3, 1, 0.07],
-		[base_enemy, 1, 3, 1, 0.07],
-		[base_enemy, 2, 4, 1, 0.07],
-		[base_enemy, 3, 4, 1, 0.07],
-	],
-	[	
-		#wave 2
-		[base_enemy, 4, 3, 1, 0.07],
-		[base_enemy, 5, 5, 1, 0.07],
-		[base_enemy, 6, 3, 1, 0.07],
-		[base_enemy, 7, 5, 1, 0.07],
-	],
-	[	
-		#wave 3
-		[base_enemy, 8, 5, 1, 0.07],
-		[base_enemy, 9, 5, 1, 0.07],
-		[base_enemy, 10, 5, 1, 0.07],
-	],
-]
+
 
 func _ready():
+	var waves = [
+		[	
+			#wave 1
+			[rapidfire_enemy, 0, 4, 1, 0.07],
+			[rapidfire_enemy, 1, 4, 1, 0.07],
+			[rapidfire_enemy, 2, 4, 1, 0.07],
+			[rapidfire_enemy, 3, 4, 1, 0.07],
+		],
+		[	
+			#wave 2
+			[rapidfire_enemy, 4, 5, 1, 0.07],
+			[rapidfire_enemy, 5, 5, 1, 0.07],
+			[rapidfire_enemy, 6, 5, 1, 0.07],
+			[rapidfire_enemy, 7, 5, 1, 0.07],
+		],
+		[	
+			#wave 3
+			[rapidfire_enemy, 8, 6, 1, 0.07],
+			[rapidfire_enemy, 9, 6, 1, 0.07],
+			[rapidfire_enemy, 10, 6, 1, 0.07],
+		],
+	]
 	enemy_manager.supply_waves(waves)
-	pause_menu.hide()
-	
-	
--- levels/level_4/level_4.gd
+++ levels/level_4/level_4.gd
@@ -1,30 +1,32 @@
 extends Node2D
+
 @onready var enemy_manager: EnemyManager = %EnemyManager
-@onready var pause_menu = $CanvasLayer/PauseMenu
-var base_enemy = preload("res://entities/enemies/BaseEnemy.tscn")
+
+var base_enemy = preload("res://entities/enemies/base_enemy/base_enemy.tscn")
+var square_enemy = preload("res://entities/enemies/square/square_enemy.tscn")
+var triangle_enemy = preload("res://entities/enemies/triangle/triangle_enemy.tscn")
+var rapidfire_enemy = preload("res://entities/enemies/rapidfire/rapidfire_enemy.tscn")
 
 ##
 #  [enemy_scene, path_id, enemies_amount, delay(s), spacing]
 #  we can provide optional array with enemy parameters at the end
 ##
 
-var waves = [
-	[	
-		#wave 1
-		[base_enemy, 0, 15, 3, 0.03],
-		[base_enemy, 1, 15, 3, 0.03],
-		[base_enemy, 2, 10, 3, 0.06],
-	],
-	[	
-		#wave 2
-		[base_enemy, 0, 15, 3, 0.03],
-		[base_enemy, 1, 15, 3, 0.03],
-		[base_enemy, 2, 10, 3, 0.06],
-	],
-]
+
 
 func _ready():
+	var waves = [
+		[	
+			#wave 1
+			[triangle_enemy, 0, 7, 2, 0.03],
+			[rapidfire_enemy, 1, 10, 1, 0.03],
+			[rapidfire_enemy, 2, 10, 1, 0.06],
+		],
+		[	
+			#wave 2
+			[rapidfire_enemy, 3, 12, 1, 0.03],
+			[rapidfire_enemy, 4, 12, 1, 0.06],
+			[triangle_enemy, 5, 8, 2, 0.03],
+		],
+	]
 	enemy_manager.supply_waves(waves)
-	pause_menu.hide()
-	
-	
